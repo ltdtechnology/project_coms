28cff394c00f7d6f37d7d8b89c962437c8bf4abf
Pratiksha Jadhav <pratikshajadhav0372@gmail.com>
2026-06-02 14:54:57 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./pages/Dashboard.jsx";
